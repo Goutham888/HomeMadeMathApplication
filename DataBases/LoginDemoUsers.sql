6279730213c267f2d33eb446a3339341facc6123
Goutham888 <46071652+Goutham888@users.noreply.github.com>
2020-12-15 14:57:53 -0500

    Project Clean Up and Refinement
    There were multiple projects held in this repository. I am cleaning it up so only the home-made-math project will be visible.

--- DataBases/LoginDemoUsers.sql
+++
@@ -1,7 +0,0 @@
-/*insert into loginattempts (username, passcode, loginsuccess, currentlogin, lastloggedin) 
-values ('Goutham', 'Mitta', 'y', TO_TIMESTAMP('2020-06-04 14:39:12',  'YYYY-MM-DD HH24:MI:SS'), TO_TIMESTAMP('2020-06-05 14:39:12', 'YYYY-MM-DD HH24:MI:SS'));
-*/
-
-/*select * from loginattempts order by currentlogin desc;*/
-
-select * from loginattempts where username = 'Goutham' order by currentlogin desc;
